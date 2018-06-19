94d4cc156fc2399994cbb5493250c32dc810f59c
zogreptile <zogreptile@gmail.com>
2018-06-19 23:01:19 +1000

    Travis CI badge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const askName = () => {
-    const username = readlineSync.question('May I have your name? ');
-    console.log(`Hi, ${username}!`);
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hi, ${username}!`);
 };
